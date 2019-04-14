f3cc045a504d8d3371619f89d26a367de0d1717e
ali <ali.7@outlook.com>
2019-04-14 21:59:55 +0300

    fix-tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -10,6 +10,6 @@ it('renders without crashing', () => {
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Ap />, div);
+  ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
