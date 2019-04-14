be10c15bb795e7e59817f76b86a16633f1f8ca3f
ali <ali.7@outlook.com>
2019-04-14 21:54:18 +0300

    failing-tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -7,3 +7,9 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Ap />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
